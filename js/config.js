c46cdcba0bc14c0b3c9d9a82561f531fe9743d76
Fernando Britto <fernandobritto@github.com>
2017-06-15 15:25:40 -0300

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,12 @@
+
+
+function setConfig(){
+    var texts = {
+        "title":"Purchasing Control"
+    };
+    document.title = texts.title;
+    document.getElementById("navTitle").innerHTML = texts.title;
+}
+
+setConfig();
+
