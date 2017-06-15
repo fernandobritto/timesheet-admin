054ae88d65d9733db9e72ce10999d26208ea4183
Fernando Britto <fernandobritto@github.com>
2017-06-15 16:19:01 -0300

    AC501

--- js/main.js
+++ js/main.js
@@ -16,11 +16,12 @@ function getTotal(list){
 function setList(list){
     var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
     for (var key in list){
-        table +='<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td>Edit | Delete</td></tr>';
+        table +='<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button class="btn btn-default" onclick="setUpdate('+key+')">Edit</button> | Delete</td></tr>';
     }
     table += '<tbody>';
     document.getElementById("listTable").innerHTML = table;
 }
+
 
 function formatDesc(desc){
     var str = desc.toLowerCase();
@@ -28,12 +29,67 @@ function formatDesc(desc){
     return str;
 }
 
+
 function formatValue(value){
     var str = parseFloat(value).toFixed(2) + "";
     str = str.replace(".",",");
     str = "$ " + str;
     return str;
 }
+
+
+function addData(){
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+
+    list.unshift({"desc":desc , "amount":amount , "value":value });
+    setList(list);
+
+}
+
+
+function setUpdate(id){
+    var obj = list[id]
+    document.getElementById("desc").value = obj.desc;
+    document.getElementById("amount").value = obj.amount;
+    document.getElementById("value").value = obj.value;
+
+    document.getElementById("btnUpdate").style.display = "inline-block";
+    document.getElementById("btnAdd").style.display = "none";
+
+    document.getElementById("inputIDUpdate").innerHTML = ' <input type="hidden" id="idUpdate" value="'+id+'">';
+    
+}
+
+
+function resetForm(){
+    document.getElementById("desc").value = "";
+    document.getElementById("amount").value = "";
+    document.getElementById("value").value = "";
+
+    document.getElementById("btnUpdate").style.display = "none";
+    document.getElementById("btnAdd").style.display = "inline-block";
+
+    document.getElementById("inputIDUpdate").innerHTML = "";
+}
+
+
+
+function updateData(){
+    var id = document.getElementById("idUpdate").value;
+
+    var desc = document.getElementById("desc").value;
+    var amount = document.getElementById("amount").value;
+    var value = document.getElementById("value").value;
+
+    list[id] = {"desc":desc, "amount":amount, "value":value};
+    resetForm();
+    setList(list);
+
+}
+
+
 
 setList(list);
 console.log(getTotal(list));
